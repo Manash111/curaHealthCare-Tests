9bc1095a74120fc154ac36032d4d5530967b3a2d
Manash111 <manashmaharjan5@gmail.com>
2025-08-03 13:19:25 +0545

    Added tests scripts for login

---
+++ tests/test_login.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from pages.login_page import LoginPage
+from pages.locators import Locators
+from selenium.webdriver.support import expected_conditions as EC
+import unittest
+
+
+class TestLogin(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless=new")
+        service = Service("C:\\Drivers\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
+        cls.driver = webdriver.Chrome(service=service,options=options)
+        cls.driver.maximize_window()
+
+    def test_valid_login(self):
+        login_page = LoginPage(self.driver)
+        login_page.driver.get(Locators.url_login)
+        login_page.login("John Doe", "ThisIsNotAPassword")
+        self.assertIn("Make Appointment", self.driver.page_source)
+
+    def test_invalid_login(self):
+        self.driver.back()
+        login_page = LoginPage(self.driver)
+        login_page.driver.get(Locators.url_login)
+
+        login_page.login("wrong", "credentials")
+        self.assertIn("Login failed", self.driver.page_source)
+
+    def test_sql_injection(self):
+        login_page = LoginPage(self.driver)
+        login_page.driver.get(Locators.url_login)
+
+        login_page.login("' OR '1'='1", "anything")
+        self.assertIn("Login failed", self.driver.page_source)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
